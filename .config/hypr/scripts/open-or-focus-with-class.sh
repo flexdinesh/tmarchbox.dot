e0e19f6d3ec311a9a7b4fafef3e2f20d4cffd2b3
Dinesh Pandiyan <flexdinesh@gmail.com>
2025-05-21 17:07:08 +1000

    hypr launch or focus script

--- .config/hypr/scripts/open-or-focus-with-class.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-appName=$1
-className=$2
-
-hyprctl clients | grep class:.$className \
- && echo hyprctl keyword workspace $( hyprctl clients | grep class..$className -B4 && hyprctl dispatch focuswindow "class:^$className" ) \
- || $appName
-
--- .config/hypr/scripts/open-or-focus.sh
+++ .config/hypr/scripts/open-or-focus.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-appName=$1
+execCommand=$1
 className=$2
 
-hyprctl clients | grep class:.$className \
- && echo hyprctl keyword workspace $( hyprctl clients | grep class..$className -B4 && hyprctl dispatch focuswindow "class:^$className" ) \
- || $appName
+MATCH=$(hyprctl clients | grep -iA5 "class:.*$className" | grep 'class:' | head -n1 | awk -F': ' '{print $2}')
+
+if [ -n "$MATCH" ]; then
+  # Focus the first matching window
+  hyprctl dispatch focuswindow "class:.*$className.*"
+else
+  $execCommand &
+fi
+
+
+# hyprctl clients | grep class:.$className \
+#  && echo hyprctl keyword workspace $( hyprctl clients | grep class..$className -B4 && hyprctl dispatch focuswindow "class:^$className" ) \
+#  || $execCommand
 
